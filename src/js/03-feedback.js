dfbbc315616aae95cc815ce19fb730b0f4231609
Serhii Manko <msa01051984@gmail.com>
2022-11-20 18:49:05 +0200

    task-8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from "lodash.throttle";
 
+const KEY = 'feedback-form-state';
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('.feedback-form input'),
+    message: document.querySelector('.feedback-form textarea'),
+    submit: document.querySelector('.feedback-form button'),
+}
+formInfo()
+
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+const objForm = {};
+
+    function onFormInput(event) {
+        objForm[event.target.name] = event.target.value
+        const message = JSON.stringify(objForm)
+        localStorage.setItem(KEY, message)
+    }
+
+function onFormSubmit(event) {
+    const infoConsol = localStorage.getItem(KEY)
+    console.log(JSON.parse(infoConsol))
+    event.preventDefault();
+
+    event.currentTarget.reset();
+    localStorage.removeItem(KEY);
+}
+
+function formInfo() {
+    const saveMessege = localStorage.getItem(KEY);
+
+    if(saveMessege) {
+        const messageInfo = JSON.parse(saveMessege)
+        console.log(messageInfo);
+        refs.email.value = messageInfo.email;
+        refs.message.value = messageInfo.message;
+    }
+}; 
